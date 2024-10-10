ac7219970fc5498d5faefe5b817e8816b57a2c22
Tchisom <Kamsiyochukwu1@gmail.com>
2024-10-10 16:42:17 +0100

    feat: Add Lisk sepolia network to hardhat config

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,8 +1,32 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.27",
-};
+const LISK_PRIVATE_KEY = vars.get("LISK_PRIVATE_KEY");
 
-export default config;
+export default {
+  solidity: "0.8.24",
+  networks: {
+    // for testnet
+    "lisk-sepolia": {
+      url: "https://rpc.sepolia-api.lisk.com/",
+      accounts: [LISK_PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
+  },
+  etherscan: {
+    // Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
+    apiKey: {
+      "lisk-sepolia": "123",
+    },
+    customChains: [
+      {
+        network: "lisk-sepolia",
+        chainId: 4202,
+        urls: {
+          apiURL: "https://sepolia-blockscout.lisk.com/api",
+          browserURL: "https://sepolia-blockscout.lisk.com/",
+        },
+      },
+    ],
+  },
+};
