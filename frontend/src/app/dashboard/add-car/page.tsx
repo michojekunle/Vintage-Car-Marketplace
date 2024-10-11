88a01394d3ce55e80260c123d33e613f70e4daf8
Yusuf Roqib <114329528+yusufroqib@users.noreply.github.com>
2024-10-11 10:33:56 +0100

    refactor dashboard layout and fix hydration error

--- frontend/src/app/dashboard/add-car/page.tsx
+++ frontend/src/app/dashboard/add-car/page.tsx
@@ -1,28 +1,9 @@
 "use client";
 import React from "react";
 import AddCarForm from "./_components/add-car-form";
-// import jsonp from 'jsonp';
 
 const AddCar = () => {
-	// "https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getYears"
 
-	// useEffect(() => {
-	// 	const fetchData = async () => {
-	// 		console.log("running")
-		
-	// 		const url = 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getYears';
-    
-	// 		jsonp(url, { name: 'callback' }, (err, response) => {
-	// 		  if (err) {
-	// 			console.log(err.message);  // Handle any errors
-	// 		  } else {
-	// 			console.log({response});      // Store the data
-	// 		  }
-	// 		});
-	// 		// console.log(data);
-	// 	};
-	// 	fetchData();
-	// }, []);
 
 	return <AddCarForm />;
 };
