03a816716b33f21d11f9653da0220e42206997a3
Donald Nwokoro <infect3dlab@gmail.com>
2024-10-11 19:59:04 +0100

    Refactor: Config for deployment and Verification

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -3,6 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
   mocha: {
     fuzz: {
       runs: 1000
