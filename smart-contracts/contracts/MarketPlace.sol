e0253702283ee8b12ff687d60b5c05327b90ac71
rozzy <rosekaremeri@gmail.com>
2024-10-11 16:25:03 +0300

    Feature: List NFT

--- smart-contracts/contracts/MarketPlace.sol
+++ smart-contracts/contracts/MarketPlace.sol
@@ -4,14 +4,32 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract NFTMarketplace {
-     struct Listing {
+    struct Listing {
         address seller;
         uint256 price;
     }
 
     //we'll reference the nft contract first, sth like IERC721 public immutable nftContract;
     uint256 public marketplaceFee = 2;
+    mapping(uint256 => Listing) public listings;
+    mapping(address => uint256) public proceeds;
+
+    constructor(IERC721 _nftContract) {
+        nftContract = _nftContract;
+    }
 
 
+    function listNFT(uint256 tokenId, uint256 price) external nonReentrant {
+        require(nftContract.ownerOf(tokenId) == msg.sender, "Not the owner");
+        require(price > 0, "Price must be greater than 0");
 
+        require(
+            nftContract.isApprovedForAll(msg.sender, address(this)) || 
+            nftContract.getApproved(tokenId) == address(this),
+            "Marketplace not approved"
+        );
+
+        listings[tokenId] = Listing(msg.sender, price);
+    
+    }
 }
