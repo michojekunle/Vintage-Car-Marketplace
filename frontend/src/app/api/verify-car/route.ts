975c181656e57f37d00184abe67d059c0664d016
AMD <michojekunle1@gmail.com>
2024-10-15 12:45:08 +0100

    added ethers protected route for dashboard

--- frontend/src/app/api/verify-car/route.ts
+++ frontend/src/app/api/verify-car/route.ts
@@ -1,16 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
+import { carVerifier } from './_helpers';
 
 export async function POST(req: NextRequest) {
-	const { isSuccessfullyMatched, idNumber, fullName } =
+	const { vin, make, model, year } =
 		await req.json();
 
+    if(!vin || !make || !model || !year) return new NextResponse(
+        JSON.stringify({ error: "Bad request, Invalid Parameters passed" }),
+        { status: 400 }
+    );
+
 	try {
-		// if (!isSuccessfullyMatched || !isNameAvailable) {
-		// 	return new NextResponse(JSON.stringify({ error: "Failed to verify" }), {
-		// 		status: 400,
-		// 	});
-		// }
-		return NextResponse.json({ message: "successfully verified" });
+        const verifiedInfo = await carVerifier(vin, make, model, year);
+
+        if(verifiedInfo?.success) {
+            console.log("Request id found successfully");
+            return NextResponse.json({ message: "successfully verified", data: verifiedInfo?.response });
+        }
 	} catch (error) {
 		console.error(error);
 		// return new Response("Internal Server Error", { status: 500 });
--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -3,9 +3,12 @@ import { ReactNode } from "react";
 
 import Sidebar from "./_components/sidebar";
 import DashboardHeader from "./_components/header";
+import { useAccount } from "wagmi";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 // import { FaceTecProvider } from "@/facetec/context/FacetecContext";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
+	const { isConnected, address } = useAccount();
 
 	return (
 		<div className="flex h-screen bg-gray-100 dark:bg-gray-900">
@@ -20,7 +23,14 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
 				{/* <FaceTecProvider> */}
 
 				<main className="flex-1 overflow-y-auto p-4 bg-gray-100 ">
-					{children}
+					{isConnected && address ? (
+						<>{children}</>
+					): (
+						<div className="flex h-full w-full justify-center items-center text-center">
+							<p>Please connect your account to explore your dashboard</p>
+							<ConnectButton/>
+						</div>
+					)}
 				</main>
 				{/* </FaceTecProvider> */}
 			</div>
