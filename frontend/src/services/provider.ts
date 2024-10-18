b6226a5415a468fbf62144fbccfeec9000890b0c
ceasermikes002 <chimaemekaiheonu@gmail.com>
2024-10-18 14:43:42 +0100

    fixed getProvider bug

--- frontend/src/services/provider.ts
+++ frontend/src/services/provider.ts
@@ -1,11 +1,26 @@
 import { ethers } from "ethers";
 
+let provider: ethers.BrowserProvider | ethers.JsonRpcProvider;
+
 export const getProvider = async () => {
-  if (typeof window !== "undefined" && window.ethereum) {
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-    return new ethers.BrowserProvider(window.ethereum);
-  } else {
-    console.warn("MetaMask not detected. Using fallback provider.");
-    return new ethers.JsonRpcProvider(process.env.NEXT_JSON_RPC_PROVIDER);
+  if (!provider) {
+    if (typeof window !== "undefined" && window.ethereum) {
+      // Lazy load the provider when the user explicitly connects
+      provider = new ethers.BrowserProvider(window.ethereum);
+    } else {
+      console.warn("MetaMask not detected. Using fallback provider.");
+      provider = new ethers.JsonRpcProvider(process.env.NEXT_JSON_RPC_PROVIDER);
+    }
+  }
+  return provider;
+};
+
+export const connectWallet = async () => {
+  try {
+    const currentProvider = await getProvider();
+    await currentProvider.send("eth_requestAccounts", []);
+    return currentProvider;
+  } catch (error) {
+    console.error("Failed to connect wallet:", error);
   }
 };
